c21a7e7ba8a620257f7024095f4d4fd57220a009
Icus101 <91389956+Icus101@users.noreply.github.com>
2023-06-22 18:11:21 +0100

    updates the speakers state

--- src/app/components/Disclosure.tsx
+++ src/app/components/Disclosure.tsx
@@ -18,14 +18,14 @@ export default function Disclosures() {
           {({ open }) => (
             <>
               {clicked ?
-                <Disclosure.Button onClick={handleClick} className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-sm font-medium text-[#0B1237] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <Disclosure.Button onClick={handleClick} className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-[16px] sm:text-[20px] font-[700] text-[#0B1237] leading-[27px]  focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                   <span>What is ABCD with Buidl?</span>
                   <CiCircleMinus onClick={handleClick}
                     className={`${open ? 'rotate-180 transform' : ''
                       } h-5 w-5 text-[#0B1237]`}
                   />
                 </Disclosure.Button> :
-                <Disclosure.Button onClick={handleClick} className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-sm font-medium text-[#0B1237] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <Disclosure.Button onClick={handleClick} className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-[16px] sm:text-[20px] font-[700] leading-[27px] text-[#0B1237] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                   <span>What is ABCD with Buidl?</span>
                   < AiOutlinePlusCircle onClick={handleClick}
                     className={`${open ? 'rotate-180 transform' : ''
@@ -34,7 +34,7 @@ export default function Disclosures() {
 
                 </Disclosure.Button>}
 
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+              <Disclosure.Panel className="px-4 pt-4 pb-2 bg-[#ffffff] font-[500] text-[16px] text-[rgba(11,18,55,0.50)]">
                 Lorem ipsum dolor sit amet consectetur.
                 Pharetra pellentesque porta quis condimentum mi morbi sapien.
                 Urna auctor tincidunt facilisis in elementum lectus scelerisque faucibus.
--- src/app/components/ImageSlider.tsx
+++ src/app/components/ImageSlider.tsx
@@ -17,6 +17,8 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, spePan }) => {
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
+    const [acceptingApplication, setApplication] = useState(false)
+
     const nextImage = () => {
         if (currentImageIndex < images.length - 1) {
             setCurrentImageIndex(currentImageIndex + 1);
@@ -57,30 +59,57 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, spePan }) => {
                 </div>
 
             </div>
-            <div className='overflow-x-scroll '>
-                <div
-                    className={` flex  relative transform  gap-4 transition-transform duration-300`}
-                    style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
-                >
-                    {images.map((image, index) => (
-                        <div key={index} className="w-[90%] sm:w-full h-auto min-w-[28%]  shrink-0 sm:flex-shrink" >
-                            <Image width={276} height={276} src={image} alt={`Image ${index}`} className="blur-sm w-full h-auto" />
+            {acceptingApplication ?
+                <div className='overflow-x-scroll '>
+                    <div
+                        className={` flex relative transform gap-0  sm:gap-4 transition-transform duration-300`}
+                    // style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
+                    >
+                        {images.map((image, index) => (
+                            <div key={index} className="w-[100%] sm:w-full h-auto min-w-[28%]  shrink-0 sm:flex-shrink" >
+                                <Image width={276} height={276} src={image} alt={`Image ${index}`} className="blur-sm w-full h-auto" />
+                            </div>
+
+                        ))}
+
+
+                        <div className='absolute lg:block z-[10] top-[20%]  lg:top-[20%] left-0 sm:left-[10%] border-[1px] bg-[#FFFFFF] w-[100%] sm:w-[75%]'>
+                            <div>
+                                <h1 className='text-[#0b1237] text-center mt-[42px] md:mt-[10px] lg:mt-[20px] xl:mt-[47px] xl:mb-[10px]  font-ppneue leading-[24.6px] lg:leading-[69.7px]  font-[800] text-[24px] lg:text-[40px]'>We are accepting applications</h1>
+                                <h2 className='relative text-center font-[500]  lg:mb-[30px] mb-[29.5px] font-satoshi text-[16px]'>Apply to be a speaker at Build:
+                                    <a href="#">< BsFillArrowRightCircleFill size='20px' fill='#2ec866' stroke="#2ec866" style={{ border: '#2ec866' }} className='absolute md:right-[20%] right-[45%]  lg:right-[28%] xl:right-[33%] sm:bottom-[2px] ' /></a> </h2>
+                            </div>
+
                         </div>
-
-                    ))}
-
-
-                    <div className='absolute lg:block z-[10] top-[20%]  lg:top-[20%] left-0 sm:left-[10%] border-[1px] bg-[#FFFFFF] w-[100%] sm:w-[75%]'>
-                        <div>
-                            <h1 className='text-[#0b1237] text-center mt-[42px] md:mt-[10px] lg:mt-[20px] xl:mt-[47px] xl:mb-[10px]  font-ppneue leading-[24.6px] lg:leading-[69.7px]  font-[800] text-[24px] lg:text-[40px]'>We are accepting applications</h1>
-                            <h2 className='relative text-center font-[500]  lg:mb-[30px] mb-[29.5px] font-satoshi text-[16px]'>Apply to be a speaker at Build:
-                            <a href="#">< BsFillArrowRightCircleFill size='20px' color="#2ec866" style={{ border: '#2ec866' }} className='absolute md:right-[20%] right-[45%]  lg:right-[28%] xl:right-[33%] sm:bottom-[2px] ' /></a> </h2>
-                        </div>
-
                     </div>
-                </div>
 
-            </div>
+                </div>
+                : <div className='overflow-x-scroll '>
+                    <div
+                        className={` flex relative transform gap-0  sm:gap-4 transition-transform duration-300`}
+                        style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
+                    >
+                        {images.map((image, index) => (
+                            <div key={index} className="w-[100%] sm:w-full h-auto min-w-[28%]  shrink-0 sm:flex-shrink" >
+                                <Image width={276} height={276} src={image} alt={`Image ${index}`} className=" w-full h-auto" />
+                            </div>
+
+                        ))}
+
+
+                        {/* <div className='absolute lg:block z-[10] top-[20%]  lg:top-[20%] left-0 sm:left-[10%] border-[1px] bg-[#FFFFFF] w-[100%] sm:w-[75%]'>
+                            <div>
+                                <h1 className='text-[#0b1237] text-center mt-[42px] md:mt-[10px] lg:mt-[20px] xl:mt-[47px] xl:mb-[10px]  font-ppneue leading-[24.6px] lg:leading-[69.7px]  font-[800] text-[24px] lg:text-[40px]'>We are accepting applications</h1>
+                                <h2 className='relative text-center font-[500]  lg:mb-[30px] mb-[29.5px] font-satoshi text-[16px]'>Apply to be a speaker at Build:
+                                    <a href="#">< BsFillArrowRightCircleFill size='20px' fill='#2ec866' stroke="#2ec866" style={{ border: '#2ec866' }} className='absolute md:right-[20%] right-[45%]  lg:right-[28%] xl:right-[33%] sm:bottom-[2px] ' /></a> </h2>
+                            </div>
+
+                        </div> */}
+                    </div>
+
+                </div>
+            }
+
 
         </div>
 
