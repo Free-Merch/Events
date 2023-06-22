38df2e8191427f0bc09b95f761c711eba40c5138
Icus101 <91389956+Icus101@users.noreply.github.com>
2023-06-22 19:27:18 +0100

    width

--- src/app/components/ImageSlider.tsx
+++ src/app/components/ImageSlider.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { BiDotsHorizontalRounded } from 'react-icons/bi';
 import { RxDotFilled } from 'react-icons/rx';
+import { css } from '@emotion/react';
 
 
 interface ImageSliderProps {
@@ -18,6 +19,21 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, spePan }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
     const [acceptingApplication, setApplication] = useState(false)
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        handleResize(); // Initial window width setup
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const nextImage = () => {
         if (currentImageIndex < images.length - 1) {
@@ -87,8 +103,8 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images, spePan }) => {
                 : <div className='overflow-x-scroll '>
                     <div
                         className={` flex  relative transform gap-0  sm:gap-4 transition-transform duration-300`}
-                        style={{ transform: `translateX(-${currentImageIndex * (window.innerWidth >= 640 ? 28 : 100)}%)` }}
-                        // transform: translateX(${currentImageIndex * (window.innerWidth >= 640 ? 10 : 100)}%);
+                        style={{ transform: `translateX(-${currentImageIndex * (windowWidth >= 640 ? 28 : 100)}%)` }}
+                    // transform: translateX(${currentImageIndex * (window.innerWidth >= 640 ? 10 : 100)}%);
                     >
                         {images.map((image, index) => (
                             <div key={index} className="w-[100%] sm:w-full h-auto min-w-[28%]  shrink-0 sm:flex-shrink" >
