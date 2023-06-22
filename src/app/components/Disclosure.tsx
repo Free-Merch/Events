ba5c9bfd5060bf7312d5375314bf2f84502b6d0d
Icus101 <91389956+Icus101@users.noreply.github.com>
2023-06-22 06:39:31 +0100

    fix bug to the disclosure

--- src/app/components/Disclosure.tsx
+++ src/app/components/Disclosure.tsx
@@ -17,17 +17,23 @@ export default function Disclosures() {
         <Disclosure>
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-sm font-medium text-[#0B1237] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is ABCD with Buidl?</span>
-                {clicked ? <CiCircleMinus onClick={handleClick}
-                  className={`${open ? 'rotate-180 transform' : ''
-                    } h-5 w-5 text-[#0B1237]`}
-                /> : <AiOutlinePlusCircle onClick={handleClick}
-                  className={`${open ? 'rotate-180 transform' : ''
-                    } h-5 w-5 text-[#0B1237]`}
-                /> }
+              {clicked ?
+                <Disclosure.Button onClick={handleClick} className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-sm font-medium text-[#0B1237] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                  <span>What is ABCD with Buidl?</span>
+                  <CiCircleMinus onClick={handleClick}
+                    className={`${open ? 'rotate-180 transform' : ''
+                      } h-5 w-5 text-[#0B1237]`}
+                  />
+                </Disclosure.Button> :
+                <Disclosure.Button onClick={handleClick} className="flex w-full justify-between rounded-lg  sm:px-4 px-0 py-2 text-left text-sm font-medium text-[#0B1237] focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                  <span>What is ABCD with Buidl?</span>
+                  < AiOutlinePlusCircle onClick={handleClick}
+                    className={`${open ? 'rotate-180 transform' : ''
+                      } h-5 w-5 text-[#0B1237]`}
+                  />
 
-              </Disclosure.Button>
+                </Disclosure.Button>}
+
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
                 Lorem ipsum dolor sit amet consectetur.
                 Pharetra pellentesque porta quis condimentum mi morbi sapien.
