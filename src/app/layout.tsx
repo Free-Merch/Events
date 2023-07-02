4104b6f556457a757d77504972357f82c12cfff8
nonseodion <nonseifebhor@gmail.com>
2023-07-02 11:55:28 +0100

    added meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -17,11 +18,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>BuidlCon</title>
+        <meta property="og:title" content="BuidlCon" key="title" />
+        <meta
+          name="description"
+          content="Attend Buidlcon and become a Web3 powerhouse."
+          key="description"
+        />
+        <meta
+          name="og:description"
+          content="Attend Buidlcon and become a Web3 powerhouse."
+          key="og:description"
+        />
+      </Head>
       <body className={inter.className}>
         <ToastContainer />
         {children}
       </body>
-
     </html>
-  )
+  );
 }
