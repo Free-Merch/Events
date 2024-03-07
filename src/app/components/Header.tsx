6784bdb45266bb8d227a3d28e56f69f5a93b9c2a
Umoh Samuel Godwin <umohsg@gmail.com>
2024-03-07 12:48:14 +0100

    ...

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,43 +13,42 @@ const style1 = { color: "#2ec866", fontSize: "60px" }
 
 const Header = () => {
     return (
-        <div className='relative '>
-            <div className="absolute sm:block hidden left-0 bottom-[30%]"><GiFingerPrint style={style} /></div>
-            <div className="absolute right-[10%] top-[15%]"><GiFingerPrint style={style1} /></div>
+        <div className='relative h-[90svh] ipad:h-[90svh] grid place-items-center '>
+            <div className="absolute sm:block hidden  left-0 bottom-[30%]"><GiFingerPrint style={style} /></div>
+            <div className="absolute sm:block hidden right-[10%] top-[15%]"><GiFingerPrint style={style1} /></div>
             <div className='absolute  bottom-[-20px] sm:bottom-[-15px] right-0'>
                 <Image className='' src='/Vector-Header2.svg' alt='vec' width={1000} height={200} />
             </div>
             <div className='absolute w-[100%]  bottom-[-20px]  sm:bottom-[-15px]  sm:left-0'>
                 <Image className=' ' src='/Vector-Header1.svg' alt='vec1' width={1500} height={1000} />
             </div>
-            <div className='container relative mx-auto h-[calc(90vh-2rem)] ipad:h-[calc(90vh-4rem)] flex flex-col justify-between'>
+            <div className='container relative mx-auto flex flex-col justify-between min-h-full'>
                 {/* <div className='border-[#000000] sm:p-[0] px-[20px]'>
                     <h1 className='text-[#000000] mt-[60px] font-[600] text-[24px] uppercase leading-[40px]'> </h1>
                 </div> */}
 
-                <div className='flex justify-center basis-[50%] items-center pt-[60px]'>
+                <div className='flex justify-center basis-[50%] items-center lpt-[60px] flex-grow'>
                     <div className='text-center  '>
-                        <div className='lpb-[40px] flex justify-center mx-auto items-center sm:w-full w-[50%]'>
+                        <div className='lpb-[40px] flex justify-center mx-auto items-center tablet:w-full w-[80%]'>
                             <div className=''>
                                 <Image src='/Buidlcon2.0.svg' alt='buildIcon' width={450} height={100} />
-
                             </div>
                         </div>
-                        <div className=' py-10' >
+                        <div className=' py-4 ipad:py-10 px-4' >
                             <h2 className='font-[400] font-ppneue text-[20px] md:text-[32px] md:leading-[46.7px] leading-[20px] tracking-[0.48px]'>Product, Community and Future of adoption.</h2>
                         </div>
                     </div>
 
                 </div>
 
-                <div className='' >
+                <div className=' flex-grow' >
                     <Countdown 
                     // eventTargetDate={targetDate} 
                     eventEndDate={endTargetDate} />
                 </div>
 
 
-                <Link href="/registration" className=' max-w-[250px] tablet:max-w-[360px] w-full mx-auto'><FullWidthButton label='Register Now' color='#0B1237'  /></Link>
+                <Link href="/registration" className=' max-w-[250px] tablet:max-w-[360px] w-full mx-auto flex-grow'><FullWidthButton label='Register Now' color='#0B1237'  /></Link>
             </div>
 
         </div>
--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { LogoIcon } from "../Icons/Custom";
 
 export const Navbar: React.FC = () => {
   const [isHidden, setIsHidden] = useState(false);
@@ -25,12 +26,15 @@ export const Navbar: React.FC = () => {
 
   return (
     <div
-      className={` py-4 ipad:py-8 pl-[10%] tablet:pl-[100px] sticky top-0 z-[999999] backdrop-blur-sm w-full transition-transform duration-500 ease-in-out ${
+      className={` py-4 pl-[10%] tablet:pl-[100px] sticky top-0 z-[999999] backdrop-blur-sm w-full transition-transform duration-500 ease-in-out ${
         isHidden ? "transform -translate-y-full" : " bg-white"
       } `}
     >
-      <Link href="/">
-        <img src="/fullLogo.svg" alt="" />
+      <Link href="/" className=" hidden tablet:block">
+        <img src="/fullLogo.svg" alt="" className=" ipad:w-32" />
+      </Link>
+      <Link href="/" className=" tablet:hidden">
+        <LogoIcon />
       </Link>
     </div>
   );
