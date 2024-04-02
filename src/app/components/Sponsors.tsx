d267893661467490c59b51130be43e4d5485b2a4
Umoh Samuel Godwin <umohsg@gmail.com>
2024-04-02 15:26:45 +0100

    sponsor logos

--- src/app/components/Sponsors.tsx
+++ src/app/components/Sponsors.tsx
@@ -3,15 +3,23 @@ import Marquee from "react-fast-marquee";
 
 const Sponsors = () => {
   return (
-    <div className="w-[100%] bg-[#F8F9FD] sm:mb-[26px] mb-[93px]">
-      <div className="container  mx-auto ">
+    <div className="w-[100%] sm:mb-[26px] mb-[93px]">
+      <h1 className=" text-center font-ppneuebold font-extrabold text-2xl text-black">
+        Sponsors
+      </h1>
+      <div className="container  mx-auto bg-[#F8F9FD] my-4 ">
         <div className="flex justify-center">
-          <div className="flex flex-wrap w-[100%]  justify-center  bg-[#F8F9FD]  my-[26px]">
+          <div className="flex flex-wrap items-center w-[100%]  justify-between  bg-[#F8F9FD]  my-[26px] overflow-y-hidden">
             <Marquee play pauseOnHover speed={50} loop={0}>
               {SponsLogoArr.map((logo, index) => (
-                <div key={index} className="relative w-[137px] h-[21px] ml-12">
+                <div
+                  key={index}
+                  className={`relative min-w-[137px] h-[50px] ml-[4.5rem]  overflow-hidden`}
+                >
                   <Image
-                    className=" block mb-[20px] sm:mb-0 object-contain"
+                    className={` block mb-[20px] sm:mb-0 object-contain min-w-[100%] ${
+                      index === 4 && "scale-125"
+                    }`}
                     src={logo}
                     alt={logo}
                     fill
@@ -35,5 +43,4 @@ const SponsLogoArr = [
   "/images/sponsor4.svg",
   "/images/sponsor5.svg",
   "/images/sponsor6.svg",
-  "/images/sponsor7.svg",
 ];
